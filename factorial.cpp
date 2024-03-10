e6e23cedc83478f34166447b0ef2947dd1de3655
Alisha Attar <159872170+AttarAlisha@users.noreply.github.com>
2024-03-10 21:25:54 +0530

    Create factorial.cpp
    C++ program to find factorial number

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, factorial = 1;
+    cout << "Enter a number: ";
+    cin >> num;
+    for (int i = 1; i <= num; ++i) {
+        factorial *= i;
+    }
+    cout << "Factorial of " << num << " is: " << factorial << endl;
+    return 0;
+}
