4e06388ab70b3dc57278967969fd95d9c543624f
Alisha Attar <159872170+AttarAlisha@users.noreply.github.com>
2024-03-24 18:57:53 +0530

    Create reverse_number.cpp
    C++ Program to Reverse a Number.

---
+++ reverse_number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int n, rev_num = 0, rem;
+
+  cout << "Enter an integer: ";
+  cin >> n;
+
+  while(n != 0) {
+    rem= n % 10;
+    rev_num = rev_num * 10 + rem;
+    n /= 10;
+  }
+
+  cout << "Reversed Number = " << rev_num;
+
+  return 0;
+}
