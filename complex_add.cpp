f1d82035dad061da1174f469e9ece128d1912fe9
Alisha Attar <159872170+AttarAlisha@users.noreply.github.com>
2024-04-22 22:08:37 +0530

    Create complex_add.cpp
    c++ program for addition of two complex nuumber.

---
+++ complex_add.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+class complex{
+	
+	public:
+	int real,imag;
+	public:	complex(){
+	real=0;
+	imag=0;
+		}
+	public: complex(int r,int i)
+	{
+		real=r;
+		imag=i;
+	}
+	
+	friend complex operator +(complex c1, complex c2);
+	
+	int getreal()
+	{
+		return real;
+	}
+	int getimag()
+	{
+		return imag;
+	}	
+};
+complex operator +(complex c1,complex c2)
+	{
+		complex c3;
+		c3.real=c1.real+c2.real;
+		c3.imag=c1.imag+c2.imag;
+		return c3;
+	}
+	
+	
+int main()
+{
+	int a,b,i1,i2;
+
+     cout<<"enter 1st complex no:  ";
+     cin>>a>>i1;
+     cout<<"enter 2nd complex no: ";
+     cin>>b,i2;
+     
+	complex c1(a,i1),c2(b,i2);
+	complex c3;
+	c3=c1+c2;
+	cout<<"Addition of two complex number is : "<<c3.getreal()<<"+"<<c3.getimag()<<"i";
+	
+	return 0;
+}
